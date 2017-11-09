723205d965c7e210e85c14aa5d325e1579b90abf
José Luis Rodríguez Fragoso <jlrodriguezf@users.noreply.github.com>
2017-11-08 19:05:52 -0600

    Modified Drawer Menu, Split Analyse into Live and From Image

--- app/src/main/java/mx/unam/cfata/labsampleanalyser/MainActivity.java
+++ app/src/main/java/mx/unam/cfata/labsampleanalyser/MainActivity.java
@@ -1,6 +1,12 @@
 package mx.unam.cfata.labsampleanalyser;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
@@ -10,8 +16,14 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
-public class MainActivity extends AnalyseOpenCVActivity implements NavigationView.OnNavigationItemSelectedListener {
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AnalyseOpenCVActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     private CharSequence mTitle;
@@ -70,10 +82,14 @@ public class MainActivity extends AnalyseOpenCVActivity implements NavigationVie
         // Navigation View Item Clicks
         int id = item.getItemId();
 
-        if (id == R.id.nav_analyse) {
+        if (id == R.id.nav_analyseLive) {
 
             Intent analyseOpenCVActivity = new Intent(MainActivity.this, AnalyseOpenCVActivity.class);
             startActivity(analyseOpenCVActivity);
+
+        } else if (id == R.id.nav_analyseStatic) {
+            Intent CameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivity(CameraIntent);
 
         } else if (id == R.id.nav_archive) {
 
@@ -81,7 +97,6 @@ public class MainActivity extends AnalyseOpenCVActivity implements NavigationVie
             ArchiveFragment archiveFragment = new ArchiveFragment();
             FragmentManager manager = getSupportFragmentManager();
             manager.beginTransaction()
-                    //.setCustomAnimations(R.anim.anim_slide_in_from_left, R.anim.anim_slide_out_from_left)
                     .replace(R.id.relLayout4F,
                             archiveFragment,
                             archiveFragment.getTag()
